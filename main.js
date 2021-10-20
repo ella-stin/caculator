5f72462080dc67d4b52fd7499a0ac4df3d244751
ella-stin <ellapark@naver.com>
2021-10-20 12:10:00 +0900

    Create html, js

---
+++ main.js
@@ -0,0 +1,23 @@
+const caculator = {
+    plus: function(a,b) {
+        console.log(a+b);
+    },
+    minus: function(a,b) {
+        console.log(a-b);
+    },
+    multi: function(a,b) {
+        console.log(a*b);
+    },
+    divide: function(a,b) {
+        console.log(a/b);
+    },
+    power: function(a,b) {
+        console.log(a**b);
+    }
+}
+
+caculator.plus(1,2);
+caculator.minus(1,2);
+caculator.multi(1,2);
+caculator.divide(1,2);
+caculator.power(1,2);
